40ce6ff179fab34f12d59f35e022dc604f8db126
tamurakeito <kit_tamtam@icloud.com>
2025-06-09 16:20:56 +0900

    routes ファイル名変更

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "@hono/node-server";
-import app from "./presentation/index";
+import app from "./presentation/routes";
 
 const port = 3000;
 console.log(`🚀 Server is running on http://localhost:${port}`);
